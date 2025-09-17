a1ffa7f6213a8124a21d671bf613c63086cfdde2
SoftwareDivision <soft@atcgroup.co.in>
2025-09-17 17:46:58 +0530

    update frontend 17 sepm

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vj94pmei488"
+    "revision": "0.ha68dv6u99o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
