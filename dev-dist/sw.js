2987720e3af437f354d9d16a0b695a91a4db79dc
imkumardeepak <deepakraznish@gmail.com>
2025-09-22 11:44:20 +0530

    fix(service-worker): update cache revision for index.html
    - Changed cache revision from "0.72gcdehhfbg" to "0.5cgkg1b9rsg"
    - Ensured service worker caches the latest index.html version
    - Maintained cleanup of outdated caches with workbox API

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.72gcdehhfbg"
+    "revision": "0.5cgkg1b9rsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
